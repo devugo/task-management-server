487c382f66a00402fb537e91b77968f16af25043
Ugochukwu Ezenwankwo <ugoezenwankwo@gmail.com>
2021-06-10 17:12:50 +0100

    Config settings for environment variables

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // Tell nestjs to run the validation pipe
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
